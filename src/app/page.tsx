92602430aeebaa97c832ba69d03d58df541d97cb
heyvj <vijayalaxmi9915@gmail.com>
2025-07-17 17:52:32 -0700

    added basic POST request  and connect via fetch api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+"use client";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold ">Hello world!</h1>;
+  return (
+    <main>
+      <p className="text-3xl font-bold ">Hello world!</p>
+      <Button
+        variant="destructive"
+        onClick={async () => {
+          const response = await fetch("./api/chat-gpt/", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              someData: true,
+            }),
+          });
+
+          console.log("RESPONSE", response);
+        }}
+      >
+        {" "}
+        Hit API{" "}
+      </Button>
+    </main>
+  );
 }
